7bb102ddd7ff027fd749fdcc17818e50ce0cbd07
Grammos <kamberigramos@gmail.com>
2015-07-02 00:50:36 +0200

    refactoring the code!

--- projekti/index.php
+++ projekti/index.php
@@ -1,12 +1,15 @@
-<?php include 'header.php' ?>
+<?php require_once('header.php');?>
 <div class="kontenti-fluid content-headeri">
-	<div id="header-content" style="padding-top:70px;">
-
-		<img src="images/about1.png" style="padding-right:90px;">
-		<img src="images/about1.png" style="padding-right:60px;">
-		<img src="images/about1.png" style="padding-right:60px;">
-	
-
+	<div id="header-content">
+		<div id="text-content">
+		<h1>Fun In The Sun</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna, congue malesuada purus dapibus at. Morbi aliquam mollis tortor at laoreet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna, congue malesuada purus dapibus at. Morbi aliquam mollis tortor at laoreet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna, congue malesuada purus dapibus at. Morbi aliquam mollis tortor at laoreet. Fusce sit amet diam in nisl faucibus tempor.</p>
+		<button class="read-more-button" style="btn">read more</button>
+		</div>
+		<div id="header-content-picture">
+		<img id="img-header" src="images/img-1.png">
+		</div>
+		
 	</div>
 </div>
 <div class="rreshti">
@@ -14,7 +17,7 @@
 		<div class="kolona9">
 			<div class="welcome">
 				<div id="welcome1">
-				<h1><font color="6c6c6c">About Sea World</font></h1>	
+				<h1><font color="6c6c6c">Welcome To Diving Center</font></h1>	
 				<p><font color="8d8d8d">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna, congue malesuada purus dapibus at. Morbi aliquam mollis tortor at laoreet. Fusce sit amet diam in nisl faucibus tempor. Aenean quis placerat massa. Integer quis nibh interdum mauris dictum commodo sed eget lectus. Etiam velit libero, mattis ut facilisis quis, aliquam id eros. Nulla scelerisque metus nec nulla placerat eget accumsan ante pretium. In eu adipiscing felis. Nulla dignissim sapien in est vehicula quis bibendum leo vulputate. Ut eleifend volutpat mauris, at aliquet lacus feugiat vel.</font></p>
 
 				<p><font color="8d8d8d">Proin laoreet nunc sit amet nisi fringilla vitae sollicitudin odio sagittis. Nam est ligula, imperdiet eu sodales quis, cursus id tellus. Sed molestie enim at est ullamcorper id tincidunt tortor molestie. Mauris orci lacus, pretium sed lacinia vitae, auctor id est. Donec ac pellentesque odio. Etiam odio felis, consectetur quis sagittis a, adipiscing suscipit nisi. Morbi justo lacus, ornare sit amet interdum ac, venenatis quis ipsum. Vestibulum ut ligula nec urna euismod placerat non vel ligula. Quisque nec ligula tellus. Duis ultrices metus non orci pulvinar sagittis congue tortor semper.</font></p>
@@ -23,24 +26,28 @@
 			</div>
 
 			<div class="services">
-				<div id="services4">
-					<h2><font color="6c6c6c">Our Clients</font></h2>
-					<div id="pic1">
-						<img style="height:105px; " src="images/about-img1.png">
-					</div>
-						<div id="kontent-services1">
-						<h2 class="paragrafi-services">Donec vel mollis urna</h2>
-						<p class="text-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna.</p>
+				<div id="services1">
+					<h2><font color="6c6c6c">New Services</font></h2>
+					<hr align="right" width="850px" size="8px" color="dbdbdb">
+				<div id="services2">
+						<div id="pic1">
+						<img style="height:105px; " src="images/img-10.png">
 						</div>
-					<div id="pic2a">
-					<img style="height:105px; " src="images/about-img1.png">
-					</div>
-					<div id="kontent-services2a">
-						<h2 class="paragrafi-services">Donec vel mollis urna</h2>
-						<p class="text-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna.</p>
+						<div id="kontent-services">
+						<h3 class="paragrafi-services">Donec vel mollis urna</h3>
+						<p class="text-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna, congue malesuada purus dapibus at. Morbi aliquam mollis tortor at laoreet. Fusce sit amet diam in nisl faucibus tempor. Aenean quis placerat massa. Integer quis nibh interdum mauris dictum commodo sed eget lectus. </p>
 						</div>
+					</div>
+					<div id="services3">
+						<div id="pic2">
+						<img style="height:105px; " src="images/img-11.png">
 
-			
+						</div>
+						<div id="kontent-services2">
+						<h3 class="paragrafi-services">Quisque enim nulla</h3>
+						<p class="text-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus gravida urna, congue malesuada purus dapibus at. Morbi aliquam mollis tortor at laoreet. Fusce sit amet diam in nisl faucibus tempor. Aenean quis placerat massa. Integer quis nibh interdum mauris dictum commodo sed eget lectus. </p>
+						</div>
+					</div>
 				</div>
 				
 			</div>
@@ -104,7 +111,7 @@
 				</div>
 			</div>
 		</div>
-		<?php include 'footer.php' ?>
+		<?php require_once('footer.php');?>
 	</div>
 </div>
 
